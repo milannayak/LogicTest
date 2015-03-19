076b2063786d07514cbd594636b4eedf277e8637
milan_zaptech <milan@zapservers.com>
2015-03-19 11:30:24 +0530

    new commit

---
+++ workspace/LogicTest/src/com/Reverse/Test.java
@@ -0,0 +1,14 @@
+package com.Reverse;
+
+public class Test {
+	public class MyTestClass {
+		
+		public static final int REQUEST = 1;
+		public String strName = "Test";
+		
+		public String strAddress = "Test Address";
+
+	}
+
+
+}
